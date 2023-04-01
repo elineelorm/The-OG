153bfb4a1bc359928d3b077e291b8914d21541a2
Hiu Sum Jaime Yue <hiusumjaimeyue@cmail.carleton.ca>
2023-03-31 20:27:00 -0400

    Add values to Home.js using a fixed dataset from Firebase

--- src/Pages/Home.js
+++ src/Pages/Home.js
@@ -1,31 +1,61 @@
 import React, { Component } from "react";
 import cookingPot from "../images/pot-icon-12.jpg";
 import logo from "../images/the-og-logo.png";
-import { auth } from '../firebase';
+import { auth, database } from '../firebase';
 import { signOut } from 'firebase/auth';
 import history from './History';
 // import { Button } from 'react-bootstrap';
-
+import { ref, set, get, update, remove, child , onValue} from "firebase/database";
 
 export default class Home extends Component {
-    handleClick(){
-        signOut(auth)
-        history.push('/Login');        
+    constructor(props) {
+        super(props);
+        this.state = {
+            safety: '',
+            state: '',
+            type: ''
+
+        }
+    }
+    componentDidMount(){
+        const dbRef = ref(database, '/Users/1/StoveManagement/stoveid/1/dataId/');
+        // const dbRef = ref(database, '/Users/1/StoveManagement');
+        onValue(dbRef, (snapshot) => {
+            const data = snapshot.val();
+            // let dataSets = [];
+
+            // snapshot.forEach(childSnapshot=>{
+            //     let childKey = childSnapshot.key;
+            //     let childValue = childSnapshot.val();
+            //     dataSets.push({"ChildKey" : childKey, "data": childValue});
+            // });
+            // this.setState({dataValues: dataSets});
+            // console.log(this.state.dataValues);
+            const currentDataset = data[2];
+            console.log(currentDataset.Safety);
+            this.setState({safety: currentDataset.Safety, state: currentDataset.State,
+                 type: currentDataset.Type});
+        })
+
+    }
+    handleClick() {
+        signOut(auth);//signout relocated went to a page only with Footer
+        history.push('/Login');
         window.location.reload();
     }
-    render(){
+    render() {
         return (
             <div className="App-body">
                 <img class="img-position" src={logo} alt="logo" />
                 <div className="row">
                     <div className="col-sm-8 border-right">
-                        <img className="cooking-pot" src={cookingPot} alt="cooking pot"/>
-                        <div className="info-display">                        
+                        <img className="cooking-pot" src={cookingPot} alt="cooking pot" />
+                        <div className="info-display">
                             <h4 className="info-title">Thermal Stove System</h4>
                             <h5>ID: </h5>
-                            <h6>Status: </h6>
-                            <h6>Previous Cooking Method: </h6>
-                            <h6>Stove System Check: </h6>
+                            <h6>Status: {this.state.safety}</h6>
+                            <h6>Previous Cooking Method: {this.state.type}</h6>
+                            <h6>Stove System Check: {this.state.state}</h6>
                         </div>
                     </div>
                     {/* <div class="d-flex" style={{height: "527px", color: "white"}}>
@@ -39,10 +69,10 @@ export default class Home extends Component {
                             </div>
                             <button type="submit">Add</button>
                         </form>
-                        <button class="dark-button" onClick={this.handleClick}>Log out</button> 
+                        <button class="dark-button" onClick={this.handleClick}>Log out</button>
                     </div>
                 </div>
-                
+
             </div>
         );
     }
